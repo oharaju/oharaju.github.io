66a66f2d537fd608bbef9b3a2132fff9e77fe887
Júlia Ohara <juliaohara.br@gmail.com>
2023-10-26 22:41:01 -0300

    feat: add color tokens

---
+++ src/tokens/colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  primary: '#51279B',
+  white: '#FFFFFF',
+  clear: '#f7f7f7',
+  light: '#7E7E7E',
+  neutral: '#cfbcf2',
+  relax: '#724BB7',
+  stamina: '#222222',
+  deep: '#9E9E9E',
+  dark: '#191919',
+};
+
+export default colors;
