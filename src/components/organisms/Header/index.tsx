fbf6ac1b7fab65bd6addc0582e756b83606e4bba
Júlia Ohara <juliaohara.br@gmail.com>
2023-10-30 21:41:11 -0300

    feat: header component creation

---
+++ src/components/organisms/Header/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+import IconsSocialNetwork from '@/components/atoms/IconSocialNetwork';
+
+export default function Header () {
+  return(
+    <Container>
+      {/* <IconsSocialNetwork/> */}
+    </Container>
+  )
+}
---
+++ src/components/organisms/Hero/index.tsx
@@ -0,0 +1,9 @@
+import BoxHero from '@/components/molecules/BoxHero';
+
+export default function Hero() {
+  return(
+    <>
+      <BoxHero/>
+    </>
+  )
+}
