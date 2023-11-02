c1710c0f2d55494d29e01447c851180980a9e90f
Júlia Ohara <juliaohara.br@gmail.com>
2023-11-01 21:41:33 -0300

    feat: add css styles global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import '@/styles/reset.css'
-import type { AppProps } from 'next/app'
+import '@/styles/reset.css';
+import '@/styles/custom.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
