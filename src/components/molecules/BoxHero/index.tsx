d1e462b4dcceccfcbe24f69a95d7f4b46e7086d5
Júlia Ohara <juliaohara.br@gmail.com>
2023-11-20 22:49:09 -0300

    feat: adjust home component

--- src/components/molecules/BoxHero/index.tsx
+++ src/components/molecules/BoxHero/index.tsx
@@ -1,10 +1,17 @@
-import { Container } from './styles';
-import IconsSocialNetwork from '@/components/atoms/IconSocialNetwork';
+import { Container, TextSmall, Title, Link } from './styles';
+import TextAnimation from '@/components/atoms/TextAnimation';
 
 export default function BoxHero () {
   return(
     <Container>
-      {/* <IconsSocialNetwork/> */}
+      <div>
+        <TextSmall>Olá, meu nome é</TextSmall>
+        <Title>Júlia Ohara</Title>
+        <TextAnimation />
+        <p>
+          Sou desenvolvedora front-end especializada na
+          construção e design de experiências digitais. Atualmente, estou trabalhando na<Link href="https://www.dnxsolucoes.com.br/" target="_blank">DNX</Link>.</p>
+      </div>
     </Container>
   )
 }
--- src/components/molecules/BoxHero/styles.ts
+++ src/components/molecules/BoxHero/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import media from '@/tokens/media';
+import colors from '@/tokens/colors';
 
 export const Container = styled.div`
   display: flex;
@@ -6,4 +8,28 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   gap: 30px;
+  padding: 0 2rem;
+  min-height: 60vh;
+
+  @media ${media.lg} {
+    padding: 0;
+  }
+`
+
+export const TextSmall = styled.p`
+  color: ${colors.secondary};
+  font-weight: 600;
+  letter-spacing: 0.02rem;
+  margin-bottom: 2rem;
+`
+
+export const Title = styled.h1`
+  font-size: clamp(30px, 8vw, 50px);
+  font-weight: 600;
+`
+
+export const Link = styled.a`
+  color: ${colors.secondary};
+  font-weight: 600;
+  margin-left: 5px;
 `
